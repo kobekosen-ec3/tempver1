3cc5c501df337ea8f5fd6e49d4cce2390d22fa81
ec3 <sutemeado.tokumei@gmail.com>
2026-04-18 15:24:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-# 確認用
+# 最新データを保存
+latest_data = {
+    "ds": "--",
+    "pico": "--",
+    "time": "--"
+}
+
+# ホーム（スマホ表示）
 @app.route("/")
 def home():
-    return "OK"
+    return render_template("index.html", data=latest_data)
 
 # データ受信
 @app.route("/data", methods=["POST"])
 def receive():
-    data = request.json
-    print("受信:", data)
+    global latest_data
+    latest_data = request.json
+    print("受信:", latest_data)
     return "OK"
 
-# Render用
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
