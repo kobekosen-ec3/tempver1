652028be5946b05df14823856da0c3cb4dee0beb
ec3 <sutemeado.tokumei@gmail.com>
2026-06-09 17:12:58 +0900

    Refactor latest_data initialization
    Removed initialization of latest_data with default values.

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ import requests
 
 app = Flask(__name__)
 
-latest_data = {
-    "ds": None,
-    "pico": None,
-    "time": "--",
-    "device":None
-}
+# latest_data = {
+#     "ds": None,
+#     "pico": None,
+#     "time": "--",
+#     "device":None
+# }
+latest_data = { }
 
 # GASのURLに変更
 GAS_URL = "https://script.google.com/macros/s/AKfycbz2zaEWHde2jAPECg80vL9zpWWfh-rl4cbGDwu4w6lbj6zIkFUSK34mqWU-3-WHJdTTdA/exec"
